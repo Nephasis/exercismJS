ec1223b786087fb291ecd5a189ef4ded1b5efe9d
Martyna Chojnacka <martyna.zaa@gmail.com>
2017-08-22 21:41:24 +0200

    Separated functions in Cipher

--- simple-cipher/simple-cipher.js
+++ simple-cipher/simple-cipher.js
@@ -1,43 +1,55 @@
-function Cipher (key) {
-    this.alphabet = "abcdefghijklmnopqrstuvwxyz".split('');
-    if (key && /^[a-z]+$/.test(key)) {
-        this.key = key
-    } else if (!/^[a-z]+$/.test(key)){
-        throw new Error('Bad key');
-    } else {
-        this.key = Array(15).join("a").split('').map((letter) => {
-            var rnumber = Math.floor(Math.random() * 25);
-            return this.alphabet[rnumber];
-        }).join('')
-    }
+const alphabet = "abcdefghijklmnopqrstuvwxyz".split('');
+
+function generateNumber (min, max) {
+  var randNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+  if (randNumber === max) {
+    randNumber = generateNumber(min, max);
+  }
+  return randNumber;
+}
+
+function numberToLetter (min, max) { 
+  return alphabet[generateNumber(min, max)];
+}
+
+function generateRandomKey (min, max, len) {
+  var key = '';
+  while (key.length < len + 1) {
+    key += numberToLetter(min, max);
+  }
+  return key;
 };
+
+function verifyBadKey (key) { 
+  return (!/^[a-z]+$/.test(key));
+};
+
+function _throw (msg) {throw new Error(msg)};
+
+function Cipher (key) {
+  verifyBadKey(key) ? _throw('Bad key') : this.key = key || generateRandomKey(0, 26, 100);
+};
+
 
 Cipher.prototype.encode = function(input) {
-
-    return input.split('').map((letter, index) => {
-        var keyNumber = this.alphabet.indexOf(this.key[index]);
-        var letterIndex = this.alphabet.indexOf(letter);
-        if (letterIndex + keyNumber > 25) {
-            var newIndex = letterIndex + keyNumber - 26;
-        } else {
-            var newIndex = letterIndex + keyNumber;
-        }
-        return this.alphabet[newIndex];
-    }).join('');
+  return input.split('').map((letter, index) => {
+      var keyNumber = alphabet.indexOf(this.key[index]),
+          letterIndex = alphabet.indexOf(letter),
+          newIndex = letterIndex + keyNumber;
+      letterIndex + keyNumber > 25 ? newIndex -= 26 : newIndex;
+      return alphabet[newIndex];
+  }).join('');
 };
-
+  
 Cipher.prototype.decode = function(input) {
-
-    return input.split('').map((letter, index) => {
-        var keyNumber = this.alphabet.indexOf(this.key[index]);
-        var letterIndex = this.alphabet.indexOf(letter);
-        if (letterIndex - keyNumber < 0) {
-            var newIndex = letterIndex - keyNumber + 26;
-        } else {
-            var newIndex = letterIndex - keyNumber;
-        }
-        return this.alphabet[newIndex];
-    }).join('');
+  return input.split('').map((letter, index) => {
+      var keyNumber = alphabet.indexOf(this.key[index]),
+          letterIndex = alphabet.indexOf(letter),
+          newIndex = letterIndex - keyNumber;
+      letterIndex - keyNumber < 0 ? newIndex += 26 : newIndex;
+      return alphabet[newIndex];
+  }).join('');
 };
+
 
 module.exports = Cipher;
