02c73fcaff602219b4caf5ab43e2990eab04378c
Martyna Chojnacka <martyna.zaa@gmail.com>
2017-09-18 17:06:45 +0200

    Fixed unclear notation in gigasecond

--- gigasecond/gigasecond.js
+++ gigasecond/gigasecond.js
@@ -3,7 +3,8 @@ function Gigasecond(birthDate) {
 };
 
 Gigasecond.prototype.date = function() {
-  return new Date(this.birthDate + 1e12);
+  let gigasecond = 1000000000000
+  return new Date(this.birthDate + gigasecond);
 }
 
 module.exports = Gigasecond;
