0f9467883f5f24bb247e25ca7810b08de27ea35e
Martyna Chojnacka <martyna.zaa@gmail.com>
2017-09-20 18:37:00 +0200

    Thrown const outside the funciton

--- gigasecond/gigasecond.js
+++ gigasecond/gigasecond.js
@@ -1,9 +1,10 @@
+const gigasecond = 1000000000000;
+
 function Gigasecond(birthDate) {
   this.birthDate = Date.parse(birthDate);
 };
 
 Gigasecond.prototype.date = function() {
-  let gigasecond = 1000000000000
   return new Date(this.birthDate + gigasecond);
 }
 
